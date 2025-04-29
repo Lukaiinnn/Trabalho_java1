7f06070cef208971c88d4c4ed8afacdcf3dc83cd
Lukaiinnn <Lucasmarquestem@gmail.com>
2025-04-29 12:24:02 -0300

    primeiro commit

---
+++ src/Produto.java
@@ -0,0 +1,15 @@
+public class Produto {
+    int id;
+    String nome;
+    String Descricao;
+    String Familia; // nao sei o tipo disso
+    String Tipo;
+    Double Lote;
+    String Altura;
+    String Largura;
+    String Comprimento;
+    Double Peso;
+    String Grau_de_fragilidade;
+    String Observacoes;
+
+}
---
+++ src/Veiculo.java
@@ -0,0 +1,83 @@
+public class Veiculo {
+    Double id;
+    String Nome_do_veiculo;
+    String Placa;
+    String Modelo;
+    String Tipo;
+    Double altura_interna;
+    Double Comprimento_interno;
+    Double Capacidade_de_peso;
+    String Observacoes;
+
+    public Double getId() {
+        return id;
+    }
+
+    public void setId(Double id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public void setPlaca(String placa) {
+        Placa = placa;
+    }
+
+    public String getNome_do_veiculo() {
+        return Nome_do_veiculo;
+    }
+
+    public void setNome_do_veiculo(String nome_do_veiculo) {
+        Nome_do_veiculo = nome_do_veiculo;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public Double getAltura_interna() {
+        return altura_interna;
+    }
+
+    public void setAltura_interna(Double altura_interna) {
+        this.altura_interna = altura_interna;
+    }
+
+    public Double getComprimento_interno() {
+        return Comprimento_interno;
+    }
+
+    public void setComprimento_interno(Double comprimento_interno) {
+        Comprimento_interno = comprimento_interno;
+    }
+
+    public Double getCapacidade_de_peso() {
+        return Capacidade_de_peso;
+    }
+
+    public void setCapacidade_de_peso(Double capacidade_de_peso) {
+        Capacidade_de_peso = capacidade_de_peso;
+    }
+
+    public String getObservacoes() {
+        return Observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        Observacoes = observacoes;
+    }
+}
